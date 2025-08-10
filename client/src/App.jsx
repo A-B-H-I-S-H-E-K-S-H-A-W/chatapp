c87bd5b119e97ac68a62da5ef1ad0bc76c6507cb
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-08-10 12:10:02 +0530

    Reimplimented the base structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,10 @@
-import { useState } from "react";
 import "./styles/globals.css";
-import { useCounterStore } from "./store/store";
-// import WhatsAppInterface from "./pages/WhatsAppInterface";
+import WhatsAppInterface from "./pages/WhatsAppInterface";
 
 function App() {
-  const count = useCounterStore((state) => state.count);
-  const increment = useCounterStore((state) => state.increment);
-  const decrement = useCounterStore((state) => state.decrement);
   return (
     <>
-      {/* <WhatsAppInterface /> */}
-      <div className="">
-        <p className="text-2xl">Counter Value: {count}</p>
-        <button onClick={increment} className="p-5 bg-green-400">
-          +
-        </button>
-        <button onClick={decrement} className="p-5 bg-red-400">
-          -
-        </button>
-      </div>
+      <WhatsAppInterface />
     </>
   );
 }
