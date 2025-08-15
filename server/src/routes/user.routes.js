4b6640baeb4a33611052d7985536938f4e7e223a
A-B-H-I-S-H-E-K-S-H-A-W <157888289+A-B-H-I-S-H-E-K-S-H-A-W@users.noreply.github.com>
2025-08-15 14:00:45 +0530

    Initilized authenticate route middleware

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { register, setUsername } from "../controllers/user.controller.js";
+import { authenticate } from "../middleware/middleware.js";
 
 export const userRouter = Router();
 
 userRouter.post("/", register);
-userRouter.post("/set-username", setUsername);
+userRouter.post("/set-username", authenticate(), setUsername);
